fa20f37fc948cf4ab4d08c89954f3333707ca64f
hppod <hppodgornik@gmail.com>
2018-11-28 00:35:23 -0200

    vetor de caracteres

--- aula-01/strings.py
+++ aula-01/strings.py
@@ -1,3 +1,16 @@
 # Trabalhando com strings
 frase = "A maldição do Python!"
 print(frase)
+
+#Vetor de caracteres
+#Indices iniciam em 0
+
+#Posição exata
+print(frase[0])
+print(frase[2])
+
+#Intervalo de posições
+print(frase[0:10])
+
+#Intervalo de posições até o final da string
+print(frase[14:])

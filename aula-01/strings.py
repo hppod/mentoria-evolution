809e338e55ed9ed62ed20e7e2b2162061e6dfde4
hppod <hppodgornik@gmail.com>
2018-11-28 00:40:19 -0200

    trabalhando com strings

--- aula-01/strings.py
+++ aula-01/strings.py
@@ -2,15 +2,35 @@
 frase = "A maldição do Python!"
 print(frase)
 
-#Vetor de caracteres
-#Indices iniciam em 0
+# Vetor de caracteres
+# Indices iniciam em 0
 
-#Posição exata
+# Posição exata
 print(frase[0])
 print(frase[2])
 
-#Intervalo de posições
+# Intervalo de posições
 print(frase[0:10])
 
-#Intervalo de posições até o final da string
+# Intervalo de posições até o final da string
 print(frase[14:])
+
+# Métodos
+# lower() - converte a string para minusculo
+print(frase.lower())
+
+#upper() - converte a string para maiusculo
+print(frase.upper())
+
+#capitalize() - converte apenas o primeiro caractere para maiusculo
+print(frase.capitalize())
+
+#islower() - retorna true caso todos os caracteres da string sejam minusuculos
+print(frase.islower())
+
+#split() - quebra a string em uma lista de caracteres
+#é possível passar parametros de quebra dentro do metodo split. se não o fizer, ele quebrara a string pelos espaços em branco
+print(frase.split())
+
+#count() - retorna a quantidade de vezes que uma string foi passada por parametro
+print(frase.count('Python'))

93f6e5ee9865444789b57ff66aaa9c14aea7c6cb
hppod <hppodgornik@gmail.com>
2018-11-28 00:19:56 -0200

    operações básicas

--- aula-01/conhecendo.py
+++ aula-01/conhecendo.py
@@ -1,3 +1,16 @@
-#Primeiro comando, print
-print ('Hello World!')
+# Primeiro comando, print
+print('Hello World!')
 
+# Operações básicas (Soma, Subtração, Multiplicação e Divisão)
+
+print("Soma: ")
+print(7 + 7)
+
+print("Subtração: ")
+print(5 - 3)
+
+print("Multiplicação: ")
+print(10 * 4)
+
+print("Divisão: ")
+print(5 / 2)

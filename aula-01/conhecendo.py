15eb36b30261d77c69210669d870708b7a287532
hppod <hppodgornik@gmail.com>
2018-11-28 00:21:59 -0200

    atribuição flexível de variáveis

--- aula-01/conhecendo.py
+++ aula-01/conhecendo.py
@@ -15,6 +15,10 @@ print(10 * 4)
 print("Divisão: ")
 print(5 / 2)
 
-#Trabalhando com variáveis
+# Trabalhando com variáveis
 nome = "Minerando Dados"
 print(nome)
+
+# Atribuição flexível de variáveis
+a, b, c = 5, 10, 15
+print(a, b, c)

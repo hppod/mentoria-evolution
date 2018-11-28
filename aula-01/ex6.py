d3ab08a93e2d5bd5a72cfa5aee7c06c0588329c6
hppod <hppodgornik@gmail.com>
2018-11-28 01:14:04 -0200

    exercicio 6

---
+++ aula-01/ex6.py
@@ -0,0 +1,15 @@
+# 6) Utilizando variáveis faça:
+
+# a) Defina uma variável com o seu nome e imprima o seu valor.
+# b) Imprima o tipo dessa variável.
+# c) Substitua o valor da sua variável nome com a concatenação do seu valor e o seu sobrenome, depois, imprima o novo valor.
+# obs: O resultado dessa variável será Nome Sobrenome, exemplo, Felipe Santana.
+# d) Defina duas variaveis com os valores do seu email e idade com apenas uma linha de comando, depois, imprima seus valores.
+
+nome = "Hygor"
+print(nome)
+print(type(nome))
+nome = "Hygor" + " " + "Podgornik"
+print(nome)
+email, idade = "hppodgornik@gmail.com", 20
+print(nome, email, idade)
---
+++ aula-01/exercicios.py
@@ -0,0 +1,19 @@
+# Exercícios Python
+
+# Imprima a frase 'Eu sou minerador de dados'
+print('Eu sou minerador de dados')
+
+# Faça operações matemáticas de soma, subtração, multiplicação e divisão
+print(15 + 32)
+print(15 - 32)
+print(15 * 32)
+print(15 / 32)
+
+# Imprima o tipo do valor 10
+print(type(10))
+
+# Converta o valor passado anteriormente para o tipo float
+print(float(10))
+
+# Converta o valor do tipo float para o tipo string
+print(str(10.0))

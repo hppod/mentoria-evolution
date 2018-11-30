1c2104bd51a289d4b63f330c14bc161de7155f30
hppod <hppodgornik@gmail.com>
2018-11-30 14:42:24 -0200

    exercicio 3

--- aula-02/ex3.py
+++ aula-02/ex3.py
@@ -5,3 +5,19 @@
 # c) Imprima as Chaves do dicionario criado anteriormente.
 # d) Imprima os valores das chaves do dicionário
 # e) Busque a chave "Felipe" se ela não existe insira esta e o valor 30 (obs: use o método setdefault())
+
+pessoas = {'Luísa': 19, 'Fulana': 18}
+
+idadeFulana = pessoas['Fulana']
+print(idadeFulana)
+
+keys = pessoas.keys()
+print(keys)
+
+values = pessoas.values()
+print(values)
+
+dflt = pessoas.setdefault('Felipe', 30)
+print(dflt)
+
+print(pessoas)

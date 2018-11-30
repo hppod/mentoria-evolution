0dc26740941af87ed0b4a0f5549c387d39f7c55a
hppod <hppodgornik@gmail.com>
2018-11-30 13:33:26 -0200

    dicionario aninhado acessando valores especificos

--- aula-02/dicionarios.py
+++ aula-02/dicionarios.py
@@ -26,8 +26,28 @@ pessoas = {
 }
 print(pessoas)
 
-#Acessando chaves especificas
+# Acessando chaves especificas
 cidfel = pessoas['Felipe']['Cidade']
 print(cidfel)
 pesfel = pessoas['Felipe']['Peso']
 print(pesfel)
+
+Cadastro_pessoas = {
+    'Clientes': {'Cliente_01': {'Nome': 'Rodrigo',
+                                'Idade': 30,
+                                'Cidade': 'Belo Horizonte',
+                                'Peso': 65
+                                },
+                 'Cliente_02': {'Nome': 'Felipe',
+                                'e-mail': 'felipe10@gmail.com'
+                                },
+                 },
+    'valores': [1, 3.5, 400, 5, 6]
+}
+
+cliente_01 = Cadastro_pessoas['Clientes']['Cliente_01']
+print(cliente_01)
+cliente_01_cidade = Cadastro_pessoas['Clientes']['Cliente_01']['Cidade']
+print(cliente_01_cidade)
+cliente_02 = Cadastro_pessoas['Clientes']['Cliente_02']
+print(cliente_02)

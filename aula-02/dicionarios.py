4a8a5eeec5b000af29bf92e56c77cdb5947f36cb
hppod <hppodgornik@gmail.com>
2018-11-30 13:23:49 -0200

    criando dicionarios

--- aula-02/dicionarios.py
+++ aula-02/dicionarios.py
@@ -2,3 +2,12 @@
 #     Objeto do tipo Chave e Valor
 #     Sintaxe: nome={'chave':'valor'}
 
+dic = {'nome': 'Rodrigo'}
+print(dic)
+
+pessoas = {
+    'Felipe': 30, 'Fulana': 18, 'Maria': 55, 'José': 80,
+    'valores': [1, 3.5, 400, 5, 6],
+    'pesos': {'Felipe': 68, 'Fulana': 55}
+}
+print(pessoas)

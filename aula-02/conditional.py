1b87e1df1349a7e19907a9ff7e6f9bcaa877f2cb
hppod <hppodgornik@gmail.com>
2018-11-30 14:15:46 -0200

    if

--- aula-02/conditional.py
+++ aula-02/conditional.py
@@ -3,3 +3,20 @@
 #     Sintaxe
 #     if<condicao>:
 #         instruções
+
+if 10 > 1:
+    print('O número é maior que 10')
+
+    # Sintaxe
+    # if<condicao>:
+    #     instruções
+    # else:
+    #     instruções
+
+x = 10
+y = 1
+
+if x > y:
+    print('O número em X é maior que em Y')
+else:
+    print('O número em Y é maior que em X')

75bfa845f895101500e3a34dfc8750ab11f145fb
hppod <hppodgornik@gmail.com>
2018-12-05 16:00:07 -0200

    imprimindo os primeiros dez registros do dataset

--- aula-05/data-analysis-houses.py
+++ aula-05/data-analysis-houses.py
@@ -1,10 +1,19 @@
+# IMPORTANDO A BIBLIOTECA PANDAS
 import pandas as pd
 
+# DEFININDO O DATASET
 dataset = pd.read_csv('kc_house_data.csv', sep=',')
 
+# VERIFICANDO O TIPO DO DATASET
 tipoDataset = type(dataset)
 print(tipoDataset)
 
+# VERIFICANDO INFORMAÇÕES DO DATASET
 dataset.info()
 
+# IMPRIMINDO O DATASET
 print(dataset)
+
+# IMPRIMINDO APENAS OS 10 PRIMEIROS REGISTROS DO DATASET
+firstTen = dataset.head(10)
+print(firstTen)

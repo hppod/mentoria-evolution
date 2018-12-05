d4700cccb076039bec27aa19c153ee4263882dbb
hppod <hppodgornik@gmail.com>
2018-12-05 16:33:14 -0200

    consultando dados nos dois datasets e juntando-os atraves da chave order_id

--- aula-05/da-querys-orders.py
+++ aula-05/da-querys-orders.py
@@ -13,3 +13,10 @@ orders_items = pd.read_csv('olist_order_items_dataset.csv')
 
 headOrderItems = orders_items.head()
 print(headOrderItems)
+
+# CONSULTANDO DADOS NOS DOIS DATASETS E JUNTANDO-OS ATRAVÉS DA CHAVER ORDER_ID
+df_query = pd.merge(orders[['order_id', 'order_status', 'order_approved_at']], orders_items[[
+                    'order_id', 'product_id', 'seller_id', 'price', 'freight_value']], on='order_id')
+
+headDfQuery = df_query.head()
+print(headDfQuery)
